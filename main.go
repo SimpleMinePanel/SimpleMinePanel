3b57019ab7eee7282186d13786fb334bb0f27f6f
matfire <mgassend@gmail.com>
2023-11-23 09:15:51 +0000

    add: tidying up + docker package

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	//"context"
+	"github.com/docker/docker/client"
+	"github.com/simpleminepanel/simpleminepanel/routes"
+	"log"
+)
+
+func main() {
+	logger := log.Default()
+
+	//dockerCtx := context.Background()
+	dockerCli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic("could not connect to docker; please make sure it is running")
+	}
+	defer dockerCli.Close()
+	router := routes.InitRouter()
+	logger.Println("SMP is starting up...")
+	router.Run()
+}
